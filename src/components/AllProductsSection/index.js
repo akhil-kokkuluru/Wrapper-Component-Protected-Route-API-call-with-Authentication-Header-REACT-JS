6b3fa2688820a13a23b5454931dfbd2b9daa236a
akhil <akhilr37@gmail.com>
2022-05-06 12:51:49 +0000

    final commit

--- src/components/AllProductsSection/index.js
+++ src/components/AllProductsSection/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import Cookies from 'js-cookie'
 
 import ProductCard from '../ProductCard'
 import './index.css'
@@ -8,8 +9,38 @@ class AllProductsSection extends Component {
     productsList: [],
   }
 
+  componentDidMount() {
+    this.productsApiCall()
+  }
+
+  productsApiCall = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const apiurl = 'https://apis.ccbp.in/products'
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(apiurl, options)
+    if (response.ok === true) {
+      const data = await response.json()
+
+      const fetchedData = data.products.map(item => ({
+        title: item.title,
+        brand: item.brand,
+        id: item.id,
+        price: item.price,
+        imageUrl: item.image_url,
+        rating: item.rating,
+      }))
+      this.setState({productsList: fetchedData})
+    }
+  }
+
   renderProductsList = () => {
     const {productsList} = this.state
+
     return (
       <div>
         <h1 className="products-list-heading">All Products</h1>
